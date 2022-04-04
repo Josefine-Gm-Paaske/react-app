91de4aa68092957cb02c7326b76954305c56a948
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-04 17:02:46 +0200

    did not spell it and forgot that i removed title

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 /**
- * Paths begins with './nameOfPath.fileType'
+ * Paths begins with './nameOfPath.'
  */
-import logo from './logo.svg';
 import './App.css';
+import POPOSList from './POPOList';
 /**
  * 
  * @returns App is the top-level component in the project
  */
-function App() {
+ function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <POPOSList />
     </div>
   );
 }
