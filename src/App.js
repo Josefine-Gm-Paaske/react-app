873ab8c30c825fbf7c573d03381dc3966a4f42b0
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-04 21:11:25 +0200

    Import React, deleted by mistake

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 /**
  * Paths begins with './nameOfPath.'
  */
-import './App.css';
-import POPOSList from './POPOSList';
-/**
- * 
- * @returns App is the top-level component in the project
- */
- function App() {
-  return (
-    <div className="App">
-      <POPOSList />
-    </div>
-  );
-}
-
-export default App;
+ import './App.css';
+ import POPOSList from './POPOSList';
+ import React from 'react';
+ /**
+  * 
+  * @returns App is the top-level component in the project
+  */
+  function App() {
+   return (
+     <div className="App">
+       <POPOSList />
+     </div>
+   );
+ }
+ 
+ export default App;
