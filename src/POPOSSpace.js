0ee51b9f282193e3f85f94fec8e27cda3bae95e1
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-04 18:30:14 +0200

    Wow i am bad at spelling.....

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -3,13 +3,15 @@
 import React from 'react'
 
 function POPOSSpace(props) {
-  const { name, image, address } = props
+  const { name, image, description } = props
   return (
     <div>
       <img src={`${process.env.PUBLIC_URL}images/${image}`} 
-      alt="Robot arm"/>
+      width="300"
+      height="300"
+      alt="..."/>
       <h1>{name}</h1>
-      <div>{address}</div>
+      <div>{description}</div>
     </div>
   )
 }
