7620cf31332d5796e366bcc03ec0d81bc56dc427
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-04 17:04:07 +0200

    created a list with attributes defined in props

---
+++ src/POPOList.js
@@ -0,0 +1,29 @@
+// src/POPOSList.js
+import './POPOSList.css';
+import React from 'react';
+import POPOSSpace from './POPOSSpace';
+
+function POPOSList() {
+    return (
+      <div className='POPOSList'>
+        <POPOSSpace
+        name="..."
+        adress="..."
+        image="..."/>
+        <POPOSSpace
+        name="..."
+        adress="..."
+        image="..."/>
+        <POPOSSpace
+        name="..."
+        adress="..."
+        image="..."/>
+        <POPOSSpace
+        name="..."
+        adress="..."
+        image="..."/>
+      </div>
+    )
+  }
+
+export default POPOSList
